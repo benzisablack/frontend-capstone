cdfcf36996cac052fe4116e4e83215eb8b5ab9ac
Nantaphong Benz <nuntapongsangchai@gmail.com>
2023-07-14 17:29:13 +0700

    Split section

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import Nav from './Nav'
+
+const Header = () => {
+  return (
+    <>
+      <img src="/public/logo192.png" alt="Logo"></img>
+      <Nav />
+    </>
+  )
+}
+
+export default Header
