e22c6367f6ae3b7321976ea672b1ee80911617a7
Nantaphong Benz <nuntapongsangchai@gmail.com>
2023-07-15 21:32:01 +0700

    add weekend special section

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import Hero from "./Hero"
+import WeekSpecials from "./WeekSpecial"
+
+const HomePage = () => {
+  return (
+    <>
+      <Hero />
+      <WeekSpecials />
+    </>
+  )
+}
+
+export default HomePage
